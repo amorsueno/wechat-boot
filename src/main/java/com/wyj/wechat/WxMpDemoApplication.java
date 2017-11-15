53064b599a125becd9355f7aaa23515d5f1f0bbf
amorsueno <15858299394@163.com>
2017-11-15 16:21:27 +0800

    提交初始化代码

---
+++ src/main/java/com/wyj/wechat/WxMpDemoApplication.java
@@ -0,0 +1,10 @@
+package com.wyj.wechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class WxMpDemoApplication {
+	 public static void main(String[] args) {
+	        SpringApplication.run(WxMpDemoApplication.class, args);
+	    }
+}
